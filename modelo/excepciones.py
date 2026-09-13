f38258a90fd4153bcb4acf6b0d6efcfe055df5db
Albert-03-sys <155678364+Albert-03-sys@users.noreply.github.com>
2026-09-12 22:19:06 -0400

    Delete modelo directory

--- modelo/excepciones.py
+++
@@ -1,7 +0,0 @@
-class EscaneoException(Exception):
-    """Excepción base para los errores del módulo de escaneo."""
-    pass
-
-class PuertoInvalidoError(EscaneoException):
-    """Lanzada cuando se intenta escanear un puerto fuera del rango 1-65535."""
-    pass
